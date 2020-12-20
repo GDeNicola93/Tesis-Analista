ca9a593729573ae0011abf9c0bc7be56c49e0fa1
Germán De Nicola <denicolagerman@gmail.com>
2020-12-20 12:36:32 -0300

    Se crea el proyecto inicial para el back

---
+++ SED-Back/src/main/java/com/sedback/SEDBack/SedBackApplication.java
@@ -0,0 +1,13 @@
+package com.sedback.SEDBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SedBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SedBackApplication.class, args);
+	}
+
+}
---
+++ SED-Back/src/test/java/com/sedback/SEDBack/SedBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sedback.SEDBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SedBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
