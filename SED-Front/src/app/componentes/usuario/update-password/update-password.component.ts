41176a2bfafa5a160aeaa04ec56dd9a07be28ab6
Germán De Nicola <denicolagerman@gmail.com>
2021-05-20 11:31:22 -0300

    Validaciones de front para cambio de contraseña de usuario

--- SED-Front/src/app/componentes/usuario/update-password/update-password.component.ts
+++ SED-Front/src/app/componentes/usuario/update-password/update-password.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CambioPassword } from 'src/app/HttpMensajes/cambio-password';
 import { UsuarioService } from 'src/app/servicios/usuario.service';
 
@@ -8,25 +9,37 @@ import { UsuarioService } from 'src/app/servicios/usuario.service';
   styleUrls: ['./update-password.component.css']
 })
 export class UpdatePasswordComponent implements OnInit {
-  cambioPassword : any = {};
+  //cambioPassword : any = {};
   mensaje = '';
   guardado = false;
   error = false;
+  updatePasswordForm : FormGroup;
 
-  constructor(private usuarioServicio:UsuarioService) { }
+  constructor(private usuarioServicio:UsuarioService,private fb : FormBuilder) { }
 
   ngOnInit(): void {
+    this.validaciones();
+  }
+
+  validaciones() : void{
+    this.updatePasswordForm = this.fb.group({
+      passwordActual : ['',Validators.required],
+      passwordNew1 : ['',Validators.required],
+      passwordNew2 : ['',Validators.required],
+    },{
+      validator: this.ConfirmedValidator('passwordNew1', 'passwordNew2')
+    });
   }
 
   guardar() : void {
     this.mensaje = '';
     this.guardado = false;
     this.error = false;
-    this.usuarioServicio.actualizarPassword(this.cambioPassword).subscribe(data => {
+    this.usuarioServicio.actualizarPassword(this.updatePasswordForm.value).subscribe(data => {
       this.mensaje = data.mensaje;
       this.guardado = true;
       this.error = false;
-      this.cambioPassword = {};
+      this.updatePasswordForm.reset()
     },
       (err: any) => {
         this.mensaje = err.error.mensaje;
@@ -34,6 +47,21 @@ export class UpdatePasswordComponent implements OnInit {
         this.error = true;
       }
     );
+  }
+
+  ConfirmedValidator(controlName: string, matchingControlName: string){
+    return (formGroup: FormGroup) => {
+        const control = formGroup.controls[controlName];
+        const matchingControl = formGroup.controls[matchingControlName];
+        if (matchingControl.errors && !matchingControl.errors.confirmedValidator) {
+            return;
+        }
+        if (control.value !== matchingControl.value) {
+            matchingControl.setErrors({ confirmedValidator: true });
+        } else {
+            matchingControl.setErrors(null);
+        }
+    }
   }
 
 }
