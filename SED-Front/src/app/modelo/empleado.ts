0d868b633c805fdeb7d620a6e67b5acaac7402e9
Germán De Nicola <denicolagerman@gmail.com>
2021-09-10 11:51:33 -0300

    Se vuelve a activar AOT ya que estaba desactivado y generaba problemas a la hora de ejecutar ng build --prod. Se resuelven algunos inconvenites de codigo ya que al activar AOT no compilaba por algunos errores en el codigo.

--- SED-Front/src/app/modelo/empleado.ts
+++ SED-Front/src/app/modelo/empleado.ts
@@ -10,19 +10,17 @@ export class Empleado {
     dni : string = "";
     email : string = "";
     fechaDeNacimiento : any;
-    estado : Estado = "";
     puestosTrabajo : PuestoTrabajo[] = [];
     usuario!: Usuario;
 
-    constructor(legajo:string,nombre:string,apellido:string,dni:string,email:string,fechaDeNacimiento : any,estado:Estado,puestosTrabajo:PuestoTrabajo[]){
-        this.legajo = legajo;
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.dni = dni;
-        this.email = email;
-        this.fechaDeNacimiento = fechaDeNacimiento;
-        this.estado = estado;
-        this.puestosTrabajo = puestosTrabajo;
-    }
+    // constructor(legajo:string,nombre:string,apellido:string,dni:string,email:string,fechaDeNacimiento : any,puestosTrabajo:PuestoTrabajo[]){
+    //     this.legajo = legajo;
+    //     this.nombre = nombre;
+    //     this.apellido = apellido;
+    //     this.dni = dni;
+    //     this.email = email;
+    //     this.fechaDeNacimiento = fechaDeNacimiento;
+    //     this.puestosTrabajo = puestosTrabajo;
+    // }
 
 }
